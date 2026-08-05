0f18fb810c314d3f5231cb28b3e55dc96a89ea77
xiedesheng <731565755@qq.com>
2026-08-05 10:11:47 +0800

    地图修改

--- src/views/northAreaLightingSys/bigGis/index.vue
+++ src/views/northAreaLightingSys/bigGis/index.vue
@@ -1,15 +1,54 @@
 <template>
-  <div class="big-gis-container">
-    大屏
+  <div class="big-gis-page">
+    <MapView ref="mapViewRef" />
+    <div class="bottom-toolbar">
+      <el-button class="toolbar-btn" @click="handleShowArea">地块</el-button>
+      <el-button class="toolbar-btn" @click="handleShowDetails">详情</el-button>
+    </div>
   </div>
 </template>
+
 <script setup lang="ts">
+import { ref } from 'vue'
+import MapView from '../comprehensivePreview/MapView.vue'
+
+const mapViewRef = ref<any>(null)
+
+function handleShowArea() {
+  mapViewRef.value?.showArea?.()
+}
+
+function handleShowDetails() {
+  mapViewRef.value?.showDetails?.()
+}
 </script>
-<style lang="less" scoped>
-  .big-gis-container {
-    width: 100%;
-    height: 100%;
-    background: #0f172a;
-    color: #fff;
-  }
+
+<style scoped>
+.big-gis-page {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background: #0f172a;
+  overflow: hidden;
+}
+
+.bottom-toolbar {
+  position: absolute;
+  left: 16px;
+  bottom: 16px;
+  z-index: 30;
+  display: flex;
+  gap: 8px;
+}
+
+.toolbar-btn {
+  background: rgba(10, 22, 40, 0.95);
+  color: #fff;
+  border: 1px solid #2a4a6f;
+}
+
+.toolbar-btn:hover {
+  background: #2a4a6f;
+  color: #fff;
+}
 </style>
